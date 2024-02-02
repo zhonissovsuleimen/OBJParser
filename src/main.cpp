13b793d4a938f453fca8f92e3863a0c08a02c978
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-02-02 02:21:50 -0500

    caching the floats

--- src/main.cpp
+++ src/main.cpp
@@ -53,18 +53,69 @@ int main(int argc, char **argv)
          }
       }
 
+      //vertex cache: x, y, z, [w]; w is optional and defaults to 1.0
+      //w is not used as the project does not require it
+      std::vector<std::vector<float>> vCache(vCount, std::vector<float>(3));
+      // for(int i = 0; i < vCount; i++) {
+      //    vCache[i][3] = 1.0;
+      // }
+
+      //texture coordinates cache: u, [v, w]; v, w are optional and default to 0.0
+      //w is not used as the project does not require it
+      std::vector<std::vector<float>> vtCache(vtCount, std::vector<float>(2));
+
+      //vertex normals cache: x, y, z;
+      std::vector<std::vector<float>> vnCache(vnCount, std::vector<float>(3));
+
       //reset the filestream to the beginning of the file
       fileStream.clear();
       fileStream.seekg(0);
 
-      //vertex cache: x, y, z, [w]; w is optional and defaults to 1.0
-      std::vector<std::vector<float>> vCache(vCount, std::vector<float>(4));
-      for(int i = 0; i < vCount; i++) {
-         vCache[i][3] = 1.0;
+      int i = 0;
+      int j = 0;
+      int k = 0;
+      while (std::getline(fileStream, line)) {
+         if (line.length() < 2) continue;
+
+         if (line[0] == 'v' && line[1] == ' ') {
+            line = line.substr(line.find(" ") + 1); 
+            
+            std::string x = line.substr(0, line.find(" "));
+            vCache[i][0] = std::stof(x);
+            line = line.substr(line.find(" ") + 1);
+            
+            std::string y = line.substr(0, line.find(" "));
+            vCache[i][1] = std::stof(y);
+            line = line.substr(line.find(" ") + 1);
+
+            std::string z = line.substr(0, line.find(" "));
+            vCache[i][2] = std::stof(z);
+
+         } else if (line[0] == 'v' && line[1] == 't') {
+            line = line.substr(line.find(" ") + 1); 
+            
+            std::string u = line.substr(0, line.find(" "));
+            vtCache[j][0] = std::stof(u);
+            line = line.substr(line.find(" ") + 1);
+            
+            std::string v = line.substr(0, line.find(" "));
+            vtCache[j][1] = std::stof(v);
+
+         } else if (line[0] == 'v' && line[1] == 'n') {
+            line = line.substr(line.find(" ") + 1); 
+            
+            std::string x = line.substr(0, line.find(" "));
+            vnCache[k][0] = std::stof(x);
+            line = line.substr(line.find(" ") + 1);
+            
+            std::string y = line.substr(0, line.find(" "));
+            vnCache[k][1] = std::stof(y);
+            line = line.substr(line.find(" ") + 1);
+
+            std::string z = line.substr(0, line.find(" "));
+            vnCache[k][2] = std::stof(z);
+         }
       }
-      //texture coordinates cache: u, [v, w]; v, w are optional and default to 0.0
-      std::vector<std::vector<float>> vtCache(vtCount, std::vector<float>(3));
-      std::vector<std::vector<float>> vnCache(vnCount, std::vector<float>(3));
 
 
       fileStream.close();
